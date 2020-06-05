c16ec0740033f79f5ab5102806543e0e152bb611
Showmick Kar <showmick12@gmail.com>
2020-06-06 00:13:26 +0600

    Create 344. Reverse String.cpp

---
+++ src/344. Reverse String.cpp
@@ -0,0 +1,10 @@
+Problem link: https://leetcode.com/problems/reverse-string/
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        for (int i = 0; i < s.size()/2; i++) {
+            swap(s[i], s[s.size() - i -1]);
+        }
+    }
+};
