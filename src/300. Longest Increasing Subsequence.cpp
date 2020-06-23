7157fb026355bf518aed46de5a84a829195e04f5
Showmick Kar <showmick12@gmail.com>
2020-06-23 15:08:56 +0600

    Update 300. Longest Increasing Subsequence.cpp

--- src/300. Longest Increasing Subsequence.cpp
+++ src/300. Longest Increasing Subsequence.cpp
@@ -1,17 +1,17 @@
 Problem link: https://leetcode.com/problems/longest-increasing-subsequence/
-Problem Difficulty: Medium.
+Problem Difficulty: Medium
 
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> length(nums.size());
+        vector<int> dp(nums.size());
         int ans = (nums.size() > 0 ? 1 : 0);
         for (int k = 0; k < nums.size(); ++k) {
-            length[k] = 1;
+            dp[k] = 1;
             for (int i = 0; i < k; ++i) {
                 if (nums[i] < nums[k]) {
-                    length[k] = max(length[k], length[i] + 1);
-                    ans = max(ans, length[k]);
+                    dp[k] = max(dp[k], dp[i] + 1);
+                    ans = max(ans, dp[k]);
                 }
             }
         }
