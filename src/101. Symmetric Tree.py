fb2d533f3679129762bc842a843716322e7a43d9
Showmick Kar <showmick12@gmail.com>
2020-12-14 00:33:13 +0600

    Update 101. Symmetric Tree.py

--- src/101. Symmetric Tree.py
+++ src/101. Symmetric Tree.py
@@ -42,3 +42,16 @@ class Solution:
             if not self.checkPalindrome(self.level[e]):
                 return False
         return True
+
+# A shorter solution using recursive property
+
+class Solution:
+    def isMirror(self, node1, node2):
+        if not node1 and not node2:
+            return True
+        if not node1 or not node2:
+            return False
+        return node1.val == node2.val and self.isMirror(node1.left, node2.right) and self.isMirror(node1.right, node2.left)
+    
+    def isSymmetric(self, root: TreeNode) -> bool:
+        return self.isMirror(root, root)
