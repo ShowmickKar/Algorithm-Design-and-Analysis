77fdbcfe45a232a029b6cb9cd21b5cee618956b9
Showmick Kar <showmick12@gmail.com>
2021-02-09 16:56:30 +0600

    Create 412. Fizz Buzz.java

---
+++ 412. Fizz Buzz.java
@@ -0,0 +1,18 @@
+/* https://leetcode.com/problems/fizz-buzz/ */
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<String>();
+        for (int i = 1; i <= n; i++) {
+        	if (i % 3 == 0 && i % 5 == 0)
+        		ans.add("FizzBuzz");
+        	else if (i % 3 == 0)
+        		ans.add("Fizz");
+        	else if (i % 5 == 0)
+        		ans.add("Buzz");
+        	else
+        		ans.add(Integer.toString(i));
+        }
+        return ans;
+    }
+}
