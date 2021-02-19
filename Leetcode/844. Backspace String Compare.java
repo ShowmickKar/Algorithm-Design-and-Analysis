3d471b38d62d0424331711cce7151ef75e46afe3
Showmick Kar <showmick12@gmail.com>
2021-02-20 00:17:19 +0600

    Update 844. Backspace String Compare.java

--- Leetcode/844. Backspace String Compare.java
+++ Leetcode/844. Backspace String Compare.java
@@ -2,37 +2,27 @@
 
 class Solution {
     public boolean backspaceCompare(String S, String T) {
+class Solution {
+    public boolean areSame(Stack<Character> s, Stack<Character> t) {
+        return s.equals(t);
+    }
+
+    public Stack<Character> buildString(String S) {
         Stack<Character> s = new Stack<Character>();
-        Stack<Character> t = new Stack<Character>();
         for (int i = 0; i < S.length(); i++) {
             if (S.charAt(i) == '#') {
-                if (s.empty() == false) s.pop();
-                else {
-                }
-                ;
-            } else s.push(S.charAt(i));
-        }
-        for (int i = 0; i < T.length(); i++) {
-            if (T.charAt(i) == '#') {
-                if (t.empty() == false) t.pop();
-                else {
-                }
-                ;
-            } else t.push(T.charAt(i));
-        }
-        try {
-            while (s.empty() == false && s.empty() == false) {
-                if (s.peek() != t.peek()) {
-                    return false;
-                }
-                s.pop();
-                t.pop();
+                if (s.empty() == false)
+                    s.pop();
+            } else {
+                s.push(S.charAt(i));
             }
-        } catch (Exception e) {
-
         }
-        if (s.empty() == true && t.empty() == true) return true;
-        if (s.empty() == true || t.empty() == true) return false;
-        return true;
+        return s;
+    }
+
+    public boolean backspaceCompare(String S, String T) {
+        Stack<Character> s = buildString(S);
+        Stack<Character> t = buildString(T);
+        return areSame(s, t);
     }
 }
