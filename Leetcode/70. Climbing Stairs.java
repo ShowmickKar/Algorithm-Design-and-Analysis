04a552887ba9596762b85359077a24fb04cfb6f9
Showmick Kar <showmick12@gmail.com>
2021-07-07 22:48:57 +0600

    Update 70. Climbing Stairs.java

--- Leetcode/70. Climbing Stairs.java
+++ Leetcode/70. Climbing Stairs.java
@@ -1,5 +1,7 @@
 /* Problem Link: https://leetcode.com/problems/climbing-stairs/ */
 
+// Iterative
+
 class Solution {
     public int climbStairs(int n) {
         int[] dp = new int[n + 1];
@@ -9,5 +11,26 @@ class Solution {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
+    }
+}
+
+// Recursive
+
+class Solution {
+    private int dfs(int n, int[] memo) {
+        if (n < 0) return 0;
+        if (n < 3) {
+            memo[n] = n;
+            return memo[n];
+        }
+        if (memo[n] < 1) {
+            memo[n] = dfs(n - 1, memo) + dfs(n - 2, memo);
+        }
+        return memo[n];
+    }
+
+    public int climbStairs(int n) {
+        int[] memo = new int[n + 1];
+        return dfs(n, memo);
     }
 }
